1653e0031cde565bcb40830d9b985df4ac1fc05c
ryota6869 <e1b19056@oit.ac.jp>
2021-11-09 15:30:07 +0900

    リポジトリの初期化

---
+++ src/test/java/oit/is/uno/auction/AuctionApplicationTests.java
@@ -0,0 +1,13 @@
+package oit.is.uno.auction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuctionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
