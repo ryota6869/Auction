3e5b6995a126e1d83237bf9f8a5b046176714e9d
ryota6869 <e1b19056@oit.ac.jp>
2021-11-16 17:00:17 +0900

    パスワードを忘れた際の処理の追加

---
+++ src/main/java/oit/is/uno/auction/model/Users.java
@@ -0,0 +1,32 @@
+package oit.is.uno.auction.model;
+
+public class Users {
+  int id;
+  String name;
+  String pass;
+
+  public int getId() {
+    return id;
+  }
+
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getPass() {
+    return pass;
+  }
+
+  public void setPass(String pass) {
+    this.pass = pass;
+  }
+
+}
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
-INSERT INTO USERS (name) VALUES ('user1');
-INSERT INTO USERS (name) VALUES ('user2');
-INSERT INTO USERS (name) VALUES ('user3');
-INSERT INTO USERS (name) VALUES ('user4');
+INSERT INTO USERS (name,pass) VALUES ('user1','p@ss1');
+INSERT INTO USERS (name,pass) VALUES ('user2','p@ss2');
+INSERT INTO USERS (name,pass) VALUES ('user3','p@ss3');
+INSERT INTO USERS (name,pass) VALUES ('user4','p@ss4');
