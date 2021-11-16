615b0ff117b2f9614f4595b1a789cef0c0a8f36b
ryota6869 <e1b19056@oit.ac.jp>
2021-11-16 23:11:03 +0900

    パスワードを忘れた際の機能の追加

--- src/main/java/oit/is/uno/auction/model/UsersMapper.java
+++ src/main/java/oit/is/uno/auction/model/UsersMapper.java
@@ -5,6 +5,6 @@ import org.apache.ibatis.annotations.Select;
 
 @Mapper
 public interface UsersMapper {
-  @Select("select pass from users where name = '#{name}'")
+  @Select("select pass from users where name = '${name}'")
   String selectPassByName(String name);
 }
