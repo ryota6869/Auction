a08cf150a922f71f1596d06154fd22b8ae395cd9
HafishD <hafishdestarky@gmail.com>
2021-11-16 15:14:04 +0900

    ログイン画面の作成

---
+++ src/main/java/oit/is/uno/auction/security/AuctionAuthConfiguration.java
@@ -0,0 +1,47 @@
+package oit.is.uno.auction.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class AuctionAuthConfiguration extends WebSecurityConfigurerAdapter {
+
+  @Override
+  protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+    // 平文のパスワードをエンコーダにかけてハッシュ化し，"user1"と関連付けている．ロール名は"USER"
+    // プログラム中に素のパスワードが含まれることになるので望ましくない
+    auth.inMemoryAuthentication().withUser("user1").password(passwordEncoder().encode("p@ss")).roles("USER");
+
+  }
+
+  @Bean
+  PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
+  }
+
+  /**
+   * 認可処理に関する設定（認証されたユーザがどこにアクセスできるか）
+   */
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+    // Spring Securityのフォームを利用してログインを行う
+    http.formLogin();
+    // http://localhost:8000/sample3 で始まるURLへのアクセスはログインが必要
+    // antMatchers().authenticated がantMatchersへのアクセスに認証を行うことを示す
+    // antMatchers()の他にanyRequest()と書くとあらゆるアクセス先を表現できる
+    // authenticated()の代わりにpermitAll()と書くと認証処理が不要であることを示す
+    http.authorizeRequests().antMatchers("/home").authenticated();
+
+    // Spring Securityの機能を利用してログアウト．ログアウト時は http://localhost:8000/ に戻る
+    http.logout().logoutSuccessUrl("/");
+  }
+
+}
