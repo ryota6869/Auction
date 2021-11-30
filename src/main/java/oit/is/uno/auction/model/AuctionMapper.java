67a421896048dde30df93bc10d5cacea6f17c2b1
HafishD <hafishdestarky@gmail.com>
2021-11-30 16:52:54 +0900

    データベース一覧を表示する

--- src/main/java/oit/is/uno/auction/model/AuctionMapper.java
+++ src/main/java/oit/is/uno/auction/model/AuctionMapper.java
@@ -7,6 +7,6 @@ import org.apache.ibatis.annotations.Select;
 
 @Mapper
 public interface AuctionMapper {
-  @Select("select auction.id, auction.maxbid, auction.date, items.name, users.name from auction, items, users where auction.itemid=items.id and auction.sellerid=users.id")
+  @Select("select auction.id, auction.maxbid, auction.date, items.name itemName, users.name sellerName from auction, items, users where auction.itemid=items.id and auction.sellerid=users.id")
   ArrayList<AuctionInfo> selectAuctionInfos();
 }
